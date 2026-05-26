43b00508ec5203c6e9346c643321420e30cc2534
baihe199989-cloud <baihe199989@gmail.com>
2026-05-27 02:26:36 +0800

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,5 @@
-export default async function handler(req, res) {
+export const maxDuration = 60;
+export const dynamic = 'force-dynamic';export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
